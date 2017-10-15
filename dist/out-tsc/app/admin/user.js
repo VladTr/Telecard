dd09c92e1e1b6805c35f95f8d86b8c8c0ae34c82
Vlad <suzirie@gmail.com>
2017-10-15 16:29:57 +0300

    new product added

--- dist/out-tsc/app/admin/user.js
+++
@@ -1,6 +0,0 @@
-export var User = (function () {
-    function User() {
-    }
-    return User;
-}());
-//# sourceMappingURL=D:/OpenServer/domains/fb/src/app/admin/user.js.map
--- dist/out-tsc/environments/environment.prod.js
+++
@@ -1,4 +0,0 @@
-export var environment = {
-    production: true
-};
-//# sourceMappingURL=D:/OpenServer/domains/fb/src/environments/environment.prod.js.map
