124590329ef78d9c241fea1e84acbab35f7695fd
Vlad <suzirie@gmail.com>
2018-08-20 22:17:22 +0300

    change firebase hosting

--- src/environments/firebase.config.ts
+++ src/environments/firebase.config.ts
@@ -1,6 +1,26 @@
+/*
 export const firebaseConfig ={
     apiKey: "AIzaSyBzyQ6rJQhOR1wd5qbNVjZU5rpDvQwYlNo",
     authDomain: "tk-test-7845f.firebaseapp.com",
     databaseURL: "https://tk-test-7845f.firebaseio.com",
     storageBucket: "tk-test-7845f.appspot.com",
 };
+*/
+
+export const firebaseConfig ={
+    apiKey: "AIzaSyCGSiNYCmqhsOtzo9aYbR_3OzQ-A_-fQY0",
+    authDomain: "telecart-950cd.firebaseapp.com",
+    databaseURL: "https://telecart-950cd.firebaseio.com",
+    storageBucket: "telecart-950cd.appspot.com",
+};
+
+
+/*
+apiKey: "AIzaSyCGSiNYCmqhsOtzo9aYbR_3OzQ-A_-fQY0",
+    authDomain: "telecart-950cd.firebaseapp.com",
+    databaseURL: "https://telecart-950cd.firebaseio.com",
+    projectId: "telecart-950cd",
+    storageBucket: "telecart-950cd.appspot.com",
+    messagingSenderId: "304735349244"
+
+    */
